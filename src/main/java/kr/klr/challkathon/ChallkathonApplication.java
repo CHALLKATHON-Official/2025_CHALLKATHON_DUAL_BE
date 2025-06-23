25718d002fcdac148c87e2fe558448d678139793
leestana01 <leestana01@naver.com>
2025-06-24 04:16:00 +0900

    init: 프로젝트 생성

---
+++ src/main/java/kr/klr/challkathon/ChallkathonApplication.java
@@ -0,0 +1,13 @@
+package kr.klr.challkathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallkathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallkathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/klr/challkathon/ChallkathonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.klr.challkathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallkathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
